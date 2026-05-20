57e8f620e1eaccd8b91548980dfcc8fb7980ccb1
Dibbo <divyajitchakraborty@gmail.com>
2026-05-21 02:06:45 +0600

    Added sendResponse utility

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express, { type Application, type Request, type Response } from "express";
+import cors from "cors"
 import globalErrorHandler from "./utility/globalErrorHandler";
 
 const app: Application = express();
 
+app.use(cors({ origin: `http://localhost:5000` })) ///???move this to ENVIRONMENT
 app.use(express.json())
 app.use(express.text()) //???
 app.use(express.urlencoded({ extended: true })) //???
